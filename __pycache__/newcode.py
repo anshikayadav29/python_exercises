6bd253286d02ad06b10c6cf080deda625a21a8ae
anshikayadav11 <148558969+anshikayadav11@users.noreply.github.com>
2025-05-16 11:17:00 +0530

    First commit

---
+++ __pycache__/newcode.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+
+host = "localhost"
+user = "root"
+password = ""  
+
+try:
+    
+    mydb = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password
+    )
+
+    cursor = mydb.cursor()
+
+    
+    cursor.execute("CREATE DATABASE school")
+    print("Database created successfully!")
+
+except mysql.connector.Error as err:
+    print(f"Database error: {err}")
