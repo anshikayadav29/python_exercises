cca6340950701d1f20ca0ed531c870167ba699d6
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-17 14:18:52 +0530

    First commit

--- school.py
+++ school.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import mysql.connector
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="1234",
     database="school"
 )
 
@@ -20,29 +19,3 @@ CREATE TABLE IF NOT EXISTS students (
 """)
 
 print("Table created successfully!")
-
-=======
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="school"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("""
-CREATE TABLE IF NOT EXISTS students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    grade VARCHAR(10)
-)
-""")
-
-print("Table created successfully!")
-
->>>>>>> ed15064c675592ec63d3867cf7974a983a160c79
-mydb.close()
