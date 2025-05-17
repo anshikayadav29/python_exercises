37632a83617b334d324c2bad8c0d578db3541307
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-17 11:16:54 +0530

    Initial commit with database project

--- __pycache__/deletequery2.py
+++ __pycache__/deletequery2.py
@@ -1,36 +1,24 @@
 import mysql.connector
 
-try:
-    # MySQL se connection
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",     # jo password config.inc.php me diya tha
-        database="school"
-    )
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",     
+    database="school"
+)
 
-    cursor = mydb.cursor()
+cursor = mydb.cursor()
 
-    # User se st_id lo jise delete karna hai
-    st_id = input("Enter student ID to delete: ")
+id = int(input("Enter student ID to delete: "))
 
-    # Query execute karo
-    query = "DELETE FROM students WHERE st_id = %s"
-    cursor.execute(query, (st_id,))
+query = "DELETE FROM students WHERE id = %s"
+cursor.execute(query, (id,))
 
-    mydb.commit()  # Changes save karo
+mydb.commit()
 
-    # Feedback do
-    if cursor.rowcount > 0:
-        print("Student deleted successfully.")
-    else:
-        print("No student found with this ID.")
+if cursor.rowcount > 0:
+    print("Student deleted successfully.")
+else:
+    print("No student found with this ID.")
 
-except mysql.connector.Error as err:
-    print("MySQL Error:", err)
-
-finally:
-    # Close connection
-    if mydb.is_connected():
-        cursor.close()
-        mydb.close()
+mydb.close()
