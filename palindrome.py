0cf5921007c926e94d5411bc4bba969a03f1be73
anshikayadav11 <anshikayadav2917@gmail.com>
2025-05-18 15:34:52 +0530

    Renamed string.py to palindrome.py to avoid module conflict

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+my_str = 'aIbohPhoBiA'
+
+# Reverse the string and compare in lowercase (optional for case-insensitivity)
+if my_str.lower() == my_str[::-1].lower():
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
+
+
